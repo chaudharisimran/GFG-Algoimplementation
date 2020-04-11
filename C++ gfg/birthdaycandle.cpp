8f9c13dbc4756ea7fc4865716b7292fa684c4f15
simc1602 <simc1602@gmail.com>
2020-04-11 10:12:10 +0530

    added defined loop

--- C++ gfg/birthdaycandle.cpp
+++ C++ gfg/birthdaycandle.cpp
@@ -1,28 +1,30 @@
 #include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define max(a, b) a > b ? a : b
+
 using namespace std;
 void Bcandels(int arr[], int n) {
     int count = 0;
     int max = 0;
-    for (int i = 0; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == max) {
-            count++;
-        }
-    }
+    rep(i, n) if (arr[i] > max) max = arr[i];
+    rep(i, n) if (arr[i] == max) count++;
     cout << count;
+}
+
+void chkAllmaxtomin(int a[]) {
+    sort(a, a + sizeof(a) / sizeof(a[0]));
+}
+
+
+int convertFive(int n) {
+
 }
 
 int main() {
     int n;
     cin >> n;
     int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
+    rep(i, n) cin >> arr[i];
     Bcandels(arr, n);
     return 0;
 }
