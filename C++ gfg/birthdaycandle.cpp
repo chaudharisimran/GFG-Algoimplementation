3c0d1f771fc9be2fdf31f91dd748c93d338fa2dd
simc1602 <simc1602@gmail.com>
2020-04-11 09:00:07 +0530

    Birthday candles blown

---
+++ C++ gfg/birthdaycandle.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Bcandels(int arr[], int n){
+    int count = 0;
+    int max = 0;
+    for (int i = 0; i < n; i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+    for (int i = 0; i < n; i++){
+        if (arr[i] == max){
+            count++;
+        }
+    }
+    cout << count;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    Bcandels(arr, n);
+    return 0;
+}
